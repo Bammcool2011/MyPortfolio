900698914ef858037d93660b090980cae5c9bb3a
Pumin Wangprasit <savee2011@hotmail.com>
2025-03-18 13:55:35 +0700

    add about my sections

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,9 +1,41 @@
 export default function About() {
     return (
-        <div className="mt-14">
+        <div>
             <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl justify-center flex text-white">
                 About
             </h1>
+            <div className="mt-10 flex flex-col md:flex-row md:justify-center">
+
+                {/* Past Section */}
+                <div className="p-4 md:w-125 mx-10">
+                    <div className="text-center text-4xl text-[#e31b6d]">
+                        Past
+                    </div>
+                    <p className="mt-6 text-center text-xl md:text-2xl text-white font-thin">
+                        I’ve always been into video games and got obsessed with modding them as a kid. Tweaking stuff was fun, but I soon figured out I needed programming skills to go further. That’s when I decided software engineering was for me.
+                    </p>
+                </div>
+
+                {/* Present Section */}
+                <div className="p-4 md:w-125 mx-10">
+                    <div className="text-center text-4xl text-[#e31b6d]">
+                        Present
+                    </div>
+                    <p className="mt-6 text-center text-xl md:text-2xl text-white font-thin">
+                        I’m studying software engineering at Stamford International University in Thailand right now. My professors and seniors are great, always encouraging me to aim higher. I built a web platform called Style-War, which was a blast and taught me tons about coding in the real world.
+                    </p>
+                </div>
+
+                {/* Future Section */}
+                <div className="p-4 md:w-125 mx-10">
+                    <div className="text-center text-4xl text-[#e31b6d]">
+                        Future
+                    </div>
+                    <p className="mt-6 text-center text-xl md:text-2xl text-white font-thin">
+                        Going forward, I want to boost my programming skills and explore AI. It’s amazing what AI can do, and I’m pumped to see how it can tackle big issues. I hope to help build something that really matters.                    </p>
+                </div>
+
+            </div>
         </div>
     );
 }
--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,41 +1,45 @@
+import { Button } from "../components/ui/button";
+
 export default function Home() {
-    return (
-      <div className="contain p-20 min-w-full min-h-full">
-  
-        <section className="flex">
-          <div className="xl:w-1/2 lg:w-full p-4 ">
-            <span className="text-4xl text-white leading-relaxed">
-              <h1>Hello! My name is</h1>
-              <h1 className="text-6xl text-white font-semibold">Pumin Whungprasit</h1>
-            </span>
-  
-            <p className="text-[#e31b6d] text-2xl mt-6 tracking-widest">
-              Junior Dev • Software Engineer
-            </p>
-  
-            <p className="text-white text-2xl font-thin tracking-wide mt-2">
-              I'm a Software Engineering graduate and junior developer.
-              I'm passionate about improving my skills in web development and have experience creating websites for university workshops
-              and contributing to game modding. I'm eager to take on new challenges and grow as a developer,
-              focusing on both frontend and backend technologies to build impactful solutions.</p>
-          </div>
-  
-          <div className="w-1/2 p-4 xl:block hidden">
-            <pre className="text-white w-[40%] mx-auto text-center font-mono whitespace-pre-wrap m-12">
-              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀
-              ⠀⠀⠀⠀⢀⡴⣆⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⣼⣿⡗⠀⠀⠀⠀
-              ⠀⠀⠀⣠⠟⠀⠘⠷⠶⠶⠶⠾⠉⢳⡄⠀⠀⠀⠀⠀⣧⣿⠀⠀⠀⠀⠀
-              ⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣤⣤⣤⣤⣤⣿⢿⣄⠀⠀⠀⠀
-              ⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠙⣷⡴⠶⣦
-              ⠀⠀⢱⡀⠀⠉⠉⠀⠀⠀⠀⠛⠃⠀⢠⡟⠀⠀⠀⢀⣀⣠⣤⠿⠞⠛⠋
-              ⣠⠾⠋⠙⣶⣤⣤⣤⣤⣤⣀⣠⣤⣾⣿⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀
-              ⠛⠒⠛⠉⠉⠀⠀⠀⣴⠟⢃⡴⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              ⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            </pre>
-          </div>
-        </section>
-  
-        
-      </div >
-    );
-  }
+  return (
+    <div className="p-20 min-w-full min-h-screen">
+      <section className="flex flex-col xl:flex-row">
+        <div className="xl:w-1/2 p-4">
+          <span className="text-4xl text-white leading-relaxed">
+            <h1>Hello! My name is</h1>
+            <h1 className="text-6xl text-white font-semibold">Pumin Whungprasit</h1>
+          </span>
+
+          <p className="text-[#e31b6d] text-2xl mt-6 tracking-widest">
+            Junior Dev • Software Engineer
+          </p>
+
+          <p className="text-white text-2xl font-thin tracking-wide mt-2">
+            I'm a Software Engineering graduate and junior developer. I'm
+            passionate about improving my skills in web development and have
+            experience creating websites for university workshops and
+            contributing to game modding. I'm eager to take on new challenges
+            and grow as a developer, focusing on both frontend and backend
+            technologies to build impactful solutions.
+          </p>
+          <Button variant="outline" className="mt-10 text-white text-xl">View my resume</Button>
+        </div>
+
+        <div className="w-full xl:w-1/2 p-6 xl:block hidden">
+          <pre className="text-white w-[40%] mx-auto text-center font-mono whitespace-pre-wrap m-12">
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀                        ⢀⡀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⢀⡴⣆⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⣼⣿⡗⠀⠀⠀⠀
+            ⠀⠀⠀⣠⠟⠀⠘⠷⠶⠶⠶⠾⠉⢳⡄⠀⠀⠀⠀⠀⣧⣿⠀⠀⠀⠀⠀
+            ⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣤⣤⣤⣤⣤⣿⢿⣄⠀⠀⠀⠀
+            ⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠙⣷⡴⠶⣦
+            ⠀⠀⢱⡀⠀⠉⠉⠀⠀⠀⠀⠛⠃⠀⢠⡟⠀⠀⠀⢀⣀⣠⣤⠿⠞⠛⠋
+            ⣠⠾⠋⠙⣶⣤⣤⣤⣤⣤⣀⣠⣤⣾⣿⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀
+            ⠛⠒⠛⠉⠉⠀⠀⠀⣴⠟⢃⡴⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          </pre>
+        </div>
+      </section>
+    </div>
+  );
+}
