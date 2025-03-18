9a45616ddb23af22e343d740edfaf688815765e2
Pumin Wangprasit <savee2011@hotmail.com>
2025-03-18 17:44:40 +0700

    add Motion-Primitives

--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,3 +1,4 @@
+import { TextLoop } from "@/components/ui/text-loop";
 import { Button } from "../components/ui/button";
 
 export default function Home() {
@@ -10,8 +11,41 @@ export default function Home() {
             <h1 className="text-5xl md:text-6xl text-white font-semibold">Pumin Whungprasit</h1>
           </span>
 
-          <p className="text-[#e31b6d] text-2xl mt-6 tracking-widest">
-            Junior Dev • Software Engineer
+          <p className='inline-flex whitespace-pre-wrap text-2xl mt-6 tracking-widest text-[#e31b6d]'>
+            Junior Dev •{' '}
+            <TextLoop
+              className='overflow-y-clip'
+              transition={{
+                type: 'spring',
+                stiffness: 900,
+                damping: 80,
+                mass: 10,
+              }}
+              variants={{
+                initial: {
+                  y: 20,
+                  rotateX: 90,
+                  opacity: 0,
+                  filter: 'blur(4px)',
+                },
+                animate: {
+                  y: 0,
+                  rotateX: 0,
+                  opacity: 1,
+                  filter: 'blur(0px)',
+                },
+                exit: {
+                  y: -20,
+                  rotateX: -90,
+                  opacity: 0,
+                  filter: 'blur(4px)',
+                },
+              }}
+            >
+              <span>Software Engineer</span>
+              <span>Developers</span>
+              <span>Designers</span>
+            </TextLoop>
           </p>
 
           <p className="text-white text-2xl font-thin tracking-wide mt-2">
--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -1,10 +1,27 @@
+import { GlowEffect } from "@/components/ui/glow-effect";
+
 export default function Projects() {
     return (
-        <div>
-            <div className="p-5 sm:p-10">
-                <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl justify-center flex text-white">
+        <div className="p-5 sm:p-10">
+            <div className="">
+                <h1 className="text-4xl font-semibold tracking-tight lg:text-5xl justify-center flex text-white">
                     Projects
                 </h1>
+            </div>
+
+            <div className="sm:p-20 p-10">
+                <div className='relative w-120'>
+                    <GlowEffect
+                        colors={['#f06292', '#c2185b', '#e91e63']}
+                        mode='colorShift'
+                        blur='soft'
+                        duration={3}
+                        scale={1.01}
+                    />
+                    <div
+                        className='relative rounded-md outline outline-1 outline-[#fff2f21f] bg-white w-[480px] h-[270px]'
+                    ></div>
+                </div>
             </div>
         </div>
     );
