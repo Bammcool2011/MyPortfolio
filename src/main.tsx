629db396b2d9d1876b9e5399013c713841acb346
bammcool2011 <savee2011@hotmail.com>
2025-05-15 15:17:31 +0700

    remove skills

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './global.css'
 import Home from './sections/home'
 import Navbar from './components/navbar'
 import About from './sections/about'
-import Skills from './sections/skills'
 import Projects from './sections/project'
 
 createRoot(document.getElementById('root')!).render(
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')!).render(
     <div className='px-12 py-14 '>
       <Home />
       <About />
-      <Skills />
       <Projects />
     </div>
   </StrictMode>,
