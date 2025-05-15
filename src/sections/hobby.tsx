56e8344e144191f0bbea015e5f4039ada724d2e1
bammcool2011 <savee2011@hotmail.com>
2025-05-15 22:49:52 +0700

    format hobbies code

--- src/sections/hobby.tsx
+++ src/sections/hobby.tsx
@@ -1,22 +1,32 @@
-import hobbyData from '../data/hobbyData.json';
+import hobbyData from "../data/hobbyData.json";
 
 export default function Hobby() {
     return (
         <div className="container mx-auto px-4">
             <div className="mb-16">
-                <h1 className="text-5xl text-white font-semibold text-center mb-4">Hobbies</h1>
+                <h1 className="text-5xl text-white font-semibold text-center mb-4">
+                    Hobbies
+                </h1>
             </div>
             <div className="flex justify-center">
                 <div className="flex flex-wrap gap-8 justify-center">
                     {hobbyData.map((hobby, idx) => (
                         <div
                             key={idx}
-                            className="bg-[#1a222c] w-full sm:w-[350px] md:w-[400px] lg:w-[500px] h-auto min-h-[180px] flex flex-col sm:flex-row items-center sm:items-center p-4 sm:p-6 rounded-lg shadow"
+                            className="bg-[#1a222c] w-full sm:w-[350px] md:w-[400px] lg:w-[500px] h-auto min-h-[180px]
+                                       flex flex-col sm:flex-row items-center sm:items-center
+                                       p-4 sm:p-6 rounded-lg shadow"
                         >
-                            <div className="text-4xl sm:text-5xl md:text-6xl mb-2 sm:mb-0 sm:mr-6 flex justify-center">{hobby.icon}</div>
+                            <div className="text-4xl sm:text-5xl md:text-6xl mb-2 sm:mb-0 sm:mr-6 flex justify-center">
+                                {hobby.icon}
+                            </div>
                             <div className="text-center sm:text-left">
-                                <h2 className="text-xl sm:text-2xl font-bold text-white mb-2">{hobby.title}</h2>
-                                <p className="text-gray-300 text-sm sm:text-base">{hobby.desc}</p>
+                                <h2 className="text-xl sm:text-2xl font-bold text-white mb-2">
+                                    {hobby.title}
+                                </h2>
+                                <p className="text-gray-300 text-sm sm:text-base">
+                                    {hobby.desc}
+                                </p>
                             </div>
                         </div>
                     ))}
