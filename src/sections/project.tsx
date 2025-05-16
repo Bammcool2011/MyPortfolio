f7d6c91d2f4db1ac44765f1a8734a5777122f5ac
bammcool2011 <savee2011@hotmail.com>
2025-05-16 15:57:10 +0700

    add card for projects

--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -1,65 +1,48 @@
-import { GlowEffect } from "@/components/ui/glow-effect";
 import projectData from "../data/projectData.json";
-
-const glowColors = ["#f06292", "#c2185b", "#e91e63"];
 
 export default function ProjectsSection() {
   return (
-    <div id='section-projects'>
-      <div id='projects'>
-        <h1 className="text-4xl font-semibold tracking-tight text-center text-white lg:text-5xl">
-          Projects
-        </h1>
-
-        <div className="flex flex-col gap-14 sm:p-20 p-10">
-          {projectData.map((project) => (
-            <div
-              key={project.title}
-              className="flex flex-col lg:flex-row items-stretch lg:gap-10 gap-8"
-            >
-              {/* Left: Image */}
-              <div className="relative w-full lg:w-1/3 flex items-center justify-center transition-transform duration-300 hover:-translate-y-2">
-                <GlowEffect
-                  colors={glowColors}
-                  mode="colorShift"
-                  blur="soft"
-                  duration={3}
-                  scale={1.01}
-                />
-                <img
-                  className="relative w-full rounded-lg"
-                  src={project.imageSrc || "/default-image.png"}
-                  alt={project.imageAlt || "Project image"}
-                />
-              </div>
-
-              {/* Right: Project Details */}
-
-              <div className="text-white flex flex-col lg:w-2/3 py-6 lg:py-2">
-                <h2 className="text-3xl font-semibold mb-2 text-center lg:text-left">{project.title}</h2>
-                <p className="text-lg font-light text-justify">{project.details}</p>
-
-                {/* Button Section */}
-                {project.links && project.links.length > 0 && (
-                  <div className="mt-4 flex flex-wrap gap-2 justify-center lg:justify-start">
-                    {project.links.map((link) => (
-                      <a
-                        key={link.label}
-                        href={link.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="btn btn-soft btn-primary "
-                      >
-                        {link.label}
-                      </a>
-                    ))}
-                  </div>
-                )}
-              </div>
+    <section id="section-projects">
+      <h1 className="text-4xl font-semibold tracking-tight text-center text-white lg:text-5xl">
+        ⌈ Projects ⌋
+      </h1>
+      <div className="flex flex-col gap-8 sm:p-20 p-10">
+        {projectData.map((project) => (
+          <div
+            key={project.title}
+            className="flex flex-col lg:flex-row items-stretch lg:gap-10 gap-8 bg-[#1a222c] rounded-xl shadow-lg p-6"
+          >
+            {/* Project Image */}
+            <div className="relative w-[450px] aspect-[3/2] flex items-center justify-center transition-transform duration-300 hover:-translate-y-2">
+              <img
+                className="w-full h-full object-cover rounded-lg"
+                src={project.imageSrc || "/default-image.png"}
+                alt={project.imageAlt || "Project image"}
+              />
             </div>
-          ))}
-        </div>
+            {/* Project Details */}
+            <div className="text-white flex flex-col lg:w-2/3 py-6 lg:py-2">
+              <h2 className="text-3xl font-semibold mb-2 text-center lg:text-left">{project.title}</h2>
+              <p className="text-lg font-light text-justify">{project.details}</p>
+              {project.links?.length > 0 && (
+                <div className="mt-4 flex flex-wrap gap-2 justify-center lg:justify-start">
+                  {project.links.map((link) => (
+                    <a
+                      key={link.label}
+                      href={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-soft btn-primary"
+                    >
+                      {link.label}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </section>
   );
 }
