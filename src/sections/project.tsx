64a8d9f1e82e4f7f344cabcab5c0028b2edbdc6d
bammcool2011 <savee2011@hotmail.com>
2025-05-16 16:05:55 +0700

    project responsive

--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -2,18 +2,22 @@ import projectData from "../data/projectData.json";
 
 export default function ProjectsSection() {
   return (
-    <section id="section-projects">
-      <h1 className="text-4xl font-semibold tracking-tight text-center text-white lg:text-5xl">
+    <section
+      id="section-projects"
+      className="flex flex-col items-center justify-center w-full min-h-screen bg-transparent"
+    >
+      <h1 className="text-4xl font-semibold tracking-tight text-center text-white lg:text-5xl mb-10">
         ⌈ Projects ⌋
       </h1>
-      <div className="flex flex-col gap-8 sm:p-20 p-10">
+
+      <div className="flex flex-col items-center gap-8 max-w-[80dvw] px-4 sm:px-8">
         {projectData.map((project) => (
           <div
             key={project.title}
-            className="flex flex-col lg:flex-row items-stretch lg:gap-10 gap-8 bg-[#1a222c] rounded-xl shadow-lg p-6"
+            className="flex flex-col items-center lg:flex-row lg:items-stretch lg:gap-10 gap-8 bg-[#1a222c] rounded-xl shadow-lg p-6 w-full"
           >
             {/* Project Image */}
-            <div className="relative w-[450px] aspect-[3/2] flex items-center justify-center transition-transform duration-300 hover:-translate-y-2">
+            <div className="relative w-full max-w-[450px] aspect-[3/2] flex items-center justify-center transition-transform duration-300 hover:-translate-y-2 mx-auto">
               <img
                 className="w-full h-full object-cover rounded-lg"
                 src={project.imageSrc || "/default-image.png"}
@@ -21,7 +25,7 @@ export default function ProjectsSection() {
               />
             </div>
             {/* Project Details */}
-            <div className="text-white flex flex-col lg:w-2/3 py-6 lg:py-2">
+            <div className="text-white flex flex-col w-full px-5 py-6 lg:py-2 items-center lg:items-start">
               <h2 className="text-3xl font-semibold mb-2 text-center lg:text-left">{project.title}</h2>
               <p className="text-lg font-light text-justify">{project.details}</p>
               {project.links?.length > 0 && (
