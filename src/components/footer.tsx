9012362e5626bac6109bf3f9d96effb09b3fffb4
bammcool2011 <savee2011@hotmail.com>
2025-05-20 04:05:40 +0700

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <footer className="text-center py-6 bg-[#0a1018] text-base text-white mt-10">
+        © 2025 Pumin WPS. All rights reserved.
+    </footer>
+);
+
+export default Footer;
