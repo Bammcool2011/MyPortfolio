274d6dca9f329df90ede36502c1f300613f890d3
Bammcool2011 <savee2011@hotmail.com>
2025-11-30 14:00:55 +0700

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,16 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "dark",
   },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+      meta: [{ name: "theme-color", content: "#ffffff" }],
+    },
+  },
 });
